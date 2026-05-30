a86727d0907bba73c056be12c34f804aa39984ba
mochizzan <izzanfirasyansyah2318@gmail.com>
2026-05-30 12:48:59 +0700

    revisi nomor

--- public/partials/view-photo/contact.php
+++ public/partials/view-photo/contact.php
@@ -16,10 +16,10 @@
             </a>
         </div>
         <div class="contact-item">
-            <a href="tel:+6281234567890" class="contact-link">
+            <a href="tel:+6281222189918" class="contact-link">
                 <div class="contact-icon"><i class="fas fa-phone"></i></div>
                 <div class="contact-label">Telepon</div>
-                <div class="contact-value">+62 812-3456-7890</div>
+                <div class="contact-value">++62 812-2218-9918</div>
             </a>
         </div>
     </div>
